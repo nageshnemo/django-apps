8b2aad10670036f9b9ac57d1a37118e19d9bf8d1
nageshnemo <nageshkumarsingh340@gmail.com>
2019-04-19 14:04:31 +0530

    injecting dynamic content from view fn to html file

--- templateproject1/testapp/views.py
+++ templateproject1/testapp/views.py
@@ -6,7 +6,10 @@ from django.shortcuts import render
 
 
 from django.http import HttpResponse
+import datetime
 # there is no need to import this
 
 def template_view(request):
-    return render(request,'testapp/results.html')
+    dt = datetime.datetime.now()
+    my_dict = {'date':dt}
+    return render(request,'testapp/results.html',context = my_dict)
