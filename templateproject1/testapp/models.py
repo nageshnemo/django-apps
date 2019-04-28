e7f1ae9bc63f084a12dbe2e2378cf304b6e3d76d
nageshnemo <nageshkumarsingh340@gmail.com>
2019-04-28 17:03:59 +0530

    end user reqst first reqst accepted by django server and forward this reqst using urls.py to views.py and views.py interact with models.py for database things and interacts wtih template folder for all the presentation logic and respinse back via django server to end user and this model is called as MVT model which is done by django

--- templateproject1/testapp/models.py
+++ templateproject1/testapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# all the database thing is done in models.def foo():
+    doc = "The  property."
+    def fget(self):
+        return self._
+    def fset(self, value):
+        self._ = value
+    def fdel(self):
+        del self._
+    return locals()
+ = property(**())
